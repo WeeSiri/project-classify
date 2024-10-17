b6b4c1d2701a5aceb606937549c58bdf3ca12112
WeeSiri <phapawee.1875@gmail.com>
2024-10-17 12:16:52 +0700

    Delete ProjectClassifyStudy directory

--- ProjectClassifyStudy/errors.php
+++
@@ -1,8 +0,0 @@
-<?php $errors = array(); ?>
-<?php if (count($errors) > 0) : ?>
-    <div class="error">
-        <?php foreach ($errors as $error) : ?>
-            <E><?php echo $error ?></E>
-        <?php endforeach ?>
-    </div>
-<?php endif ?>
